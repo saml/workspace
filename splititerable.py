b9130f693ed69a120cdd34cfc6e45bc501ddf49b
saml <skynare@gmail.com>
2014-04-16 17:26:58 -0400

    asdf

--- splititerable.py
+++ splititerable.py
@@ -1,18 +1,19 @@
 import itertools
 import io
 
+
 def split_at(iterable, n):
     '''returns two iterables: first n elements and the rest'''
-    first = itertools.islice(iterable, n)
-    rest = itertools.islice(iterable,n,None)
-    return first,rest
+    env = {'count':0}
+    def keyfunc(x):
+        env['count'] += 1
+        return env['count'] <= n
 
-data = io.BytesIO(bytes('1234567890'))
-head,tail = split_at(iter(lambda: data.read(1),"") ,3)
+    groups = itertools.groupby(iterable, keyfunc)
+    return next(groups)[1],next(groups)[1]
 
-for x in head:
-    print(x)
-print('--')
-for x in tail:
-    print(x)
+if __name__ == '__main__':
+    data = io.BytesIO(bytes('1234567890'))
+    head,tail = split_at(iter(lambda: data.read(1),"") ,3)
 
+    print('|'.join((','.join(tail), ','.join(head))))
